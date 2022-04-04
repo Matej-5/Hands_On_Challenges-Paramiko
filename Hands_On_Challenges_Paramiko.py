6eaaef6176416c2c6a2de311f4ec421741febb84
Matej-5 <matbel900@gmail.com>
2022-04-04 11:41:36 +0200

    cisco device information

---
+++ Hands_On_Challenges_Paramiko.py
@@ -0,0 +1,10 @@
+from netmiko import ConnectHandler
+
+cisco_device = {
+    'device_type': 'cisco_ios',
+    'ip_address': '10.1.1.10',
+    'username': 'u1',
+    'password': 'cisco',
+    'port': '22',
+    'verbose': True
+}
