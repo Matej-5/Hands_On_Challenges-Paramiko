cfc7dc66d4717fb7c100a16c0b217190289bb42d
Matej-5 <matbel900@gmail.com>
2022-04-04 11:45:09 +0200

    ip_address change to host for issue

--- Hands_On_Challenges_Paramiko.py
+++ Hands_On_Challenges_Paramiko.py
@@ -2,7 +2,7 @@ from netmiko import ConnectHandler
 
 cisco_device = {
     'device_type': 'cisco_ios',
-    'ip_address': '10.1.1.10',
+    'host': '10.1.1.10',
     'username': 'u1',
     'password': 'cisco',
     'port': '22',
