c7374ee794acb5ee875d4d98e29dd8376f69d87a
Matej-5 <matbel900@gmail.com>
2022-04-04 12:17:48 +0200

    change password from getpass to cisco, new file 'show_users'

--- Hands_On_Challenges_Paramiko.py
+++ Hands_On_Challenges_Paramiko.py
@@ -12,8 +12,8 @@
 # connection = ConnectHandler(**cisco_device)
 # output = connection.send_command('show arp')
 # print(output)
-
-
+#
+#
 import paramiko
 import getpass
 import time
@@ -22,15 +22,23 @@ ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 print('Connecting to client')
-my_password = getpass.getpass('Enter your password:')
-ssh_client.connect(hostname='10.1.1.10', port= '22', username='u1', password=my_password, look_for_keys=False, allow_agent=False)
+#my_password = getpass.getpass('Enter your password:')
+ssh_client.connect(hostname='10.1.1.10', port= '22', username='u1', password='cisco', look_for_keys=False, allow_agent=False)
 
 shell = ssh_client.invoke_shell()
 shell.send('show users\n')
 time.sleep(1)
 
 output = shell.recv(10000).decode()
-print(output)
+
+with open('show_users.txt', 'w') as f:
+    f.write(output)
+
+if ssh_client.get_transport().is_active():
+    print('Closing connection')
+    ssh_client.close()
+
+
 
 
 
