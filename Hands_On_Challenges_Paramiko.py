f7d42a41b41ef99c71c201374d9b8ebe6df5d8b4
Matej-5 <matbel900@gmail.com>
2022-04-04 12:46:42 +0200

    commands for sh config, w output to 'Output_2.txt'

--- Hands_On_Challenges_Paramiko.py
+++ Hands_On_Challenges_Paramiko.py
@@ -45,10 +45,23 @@ import time
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname='10.1.1.10', port= '22', username='u1', password=my_password, look_for_keys=False, allow_agent=False)
+ssh_client.connect(hostname='10.1.1.10', port= '22', username='u1', password='cisco', look_for_keys=False, allow_agent=False)
 
+shell = ssh_client.invoke_shell()
+shell.send('enable\n')
+shell.send('cisco\n')
+shell.send('terminal length 0\n')
+shell.send('show running-config\n')
+time.sleep(1)
 
+output = shell.recv(100000).decode()
 
+with open('Outputs_2.txt', 'w') as f:
+    f.write(output)
+
+if ssh_client.get_transport().is_active():
+    print('Closing connection')
+    ssh_client.close()
 
 
 
