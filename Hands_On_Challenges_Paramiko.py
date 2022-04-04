a55a22d170546ff89c17092362f2ee032efc1faf
Matej-5 <matbel900@gmail.com>
2022-04-04 12:42:47 +0200

    add ssh client

--- Hands_On_Challenges_Paramiko.py
+++ Hands_On_Challenges_Paramiko.py
@@ -14,30 +14,40 @@
 # print(output)
 
 
+# import paramiko
+# #import getpass
+# import time
+#
+# ssh_client = paramiko.SSHClient()
+# ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#
+# #print('Connecting to client')
+# #my_password = getpass.getpass('Enter your password:')
+#
+# router = {'hostname': '10.1.1.10', 'port' :  '22', 'username': 'u1', 'password': 'cisco', 'look_for_keys': False, 'allow_agent': False}
+# print(f'Connection to {router ["hostname"]}')
+# ssh_client.connect(**router)
+#
+# shell = ssh_client.invoke_shell()
+# shell.send('show users\n')
+# time.sleep(1)
+#
+# output = shell.recv(10000).decode()
+# print(output)
+#
+# if ssh_client.get_transport().is_active():
+#     print('Closing connection')
+#     ssh_client.close()
+
+
 import paramiko
-#import getpass
 import time
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname='10.1.1.10', port= '22', username='u1', password=my_password, look_for_keys=False, allow_agent=False)
 
-#print('Connecting to client')
-#my_password = getpass.getpass('Enter your password:')
 
-router = {'hostname': '10.1.1.10', 'port' :  '22', 'username': 'u1', 'password': 'cisco', 'look_for_keys': False, 'allow_agent': False}
-print(f'Connection to {router ["hostname"]}')
-ssh_client.connect(**router)
-
-shell = ssh_client.invoke_shell()
-shell.send('show users\n')
-time.sleep(1)
-
-output = shell.recv(10000).decode()
-print(output)
-
-if ssh_client.get_transport().is_active():
-    print('Closing connection')
-    ssh_client.close()
 
 
 
