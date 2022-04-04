15356e5a152b6c8a91c2e52672eb0a6e410efeae
Matej-5 <matbel900@gmail.com>
2022-04-04 12:03:43 +0200

    data to connect, command 'shell users' and output

--- Hands_On_Challenges_Paramiko.py
+++ Hands_On_Challenges_Paramiko.py
@@ -21,6 +21,17 @@ import time
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+print('Connecting to client')
+my_password = getpass.getpass('Enter your password:')
+ssh_client.connect(hostname='10.1.1.10', port= '22', username='u1', password=my_password, look_for_keys=False, allow_agent=False)
+
+shell = ssh_client.invoke_shell()
+shell.send('show users\n')
+time.sleep(1)
+
+output = shell.recv(10000).decode()
+print(output)
+
 
 
 
