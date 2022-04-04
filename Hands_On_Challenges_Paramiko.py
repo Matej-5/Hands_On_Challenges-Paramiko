7ca9e969a87ff8db86cc8ddc9687fa7e1fba381c
Matej-5 <matbel900@gmail.com>
2022-04-04 11:54:50 +0200

    create client object

--- Hands_On_Challenges_Paramiko.py
+++ Hands_On_Challenges_Paramiko.py
@@ -1,15 +1,30 @@
-from netmiko import ConnectHandler
+# from netmiko import ConnectHandler
+#
+# cisco_device = {
+#     'device_type': 'cisco_ios',
+#     'host': '10.1.1.10',
+#     'username': 'u1',
+#     'password': 'cisco',
+#     'port': '22',
+#     'verbose': True
+# }
+#
+# connection = ConnectHandler(**cisco_device)
+# output = connection.send_command('show arp')
+# print(output)
 
-cisco_device = {
-    'device_type': 'cisco_ios',
-    'host': '10.1.1.10',
-    'username': 'u1',
-    'password': 'cisco',
-    'port': '22',
-    'verbose': True
-}
 
-connection = ConnectHandler(**cisco_device)
-output = connection.send_command('show arp')
-print(output)
+import paramiko
+import getpass
+import time
+
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+
+
+
+
+
+
 
