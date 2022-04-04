8dfce39d054ea029f2b9c55fafa449bb49a25d0d
Matej-5 <matbel900@gmail.com>
2022-04-04 12:39:02 +0200

    changing argument,new item 'router', **route

--- Hands_On_Challenges_Paramiko.py
+++ Hands_On_Challenges_Paramiko.py
@@ -12,31 +12,35 @@
 # connection = ConnectHandler(**cisco_device)
 # output = connection.send_command('show arp')
 # print(output)
-#
-#
+
+
 import paramiko
-import getpass
+#import getpass
 import time
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-print('Connecting to client')
+#print('Connecting to client')
 #my_password = getpass.getpass('Enter your password:')
-ssh_client.connect(hostname='10.1.1.10', port= '22', username='u1', password='cisco', look_for_keys=False, allow_agent=False)
+
+router = {'hostname': '10.1.1.10', 'port' :  '22', 'username': 'u1', 'password': 'cisco', 'look_for_keys': False, 'allow_agent': False}
+print(f'Connection to {router ["hostname"]}')
+ssh_client.connect(**router)
 
 shell = ssh_client.invoke_shell()
 shell.send('show users\n')
 time.sleep(1)
 
 output = shell.recv(10000).decode()
-
-with open('show_users.txt', 'w') as f:
-    f.write(output)
+print(output)
 
 if ssh_client.get_transport().is_active():
     print('Closing connection')
     ssh_client.close()
+
+
+
 
 
 
